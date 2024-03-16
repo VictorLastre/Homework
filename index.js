11ee89fd435554d540f9cf2f782063711d3b1150
VictorLastre <117042793+VictorLastre@users.noreply.github.com>
2024-03-16 18:07:31 -0300

    first commit

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-function laCajaDePandora(numero) {
-  // proximamente escribiremos codigo aqui
+function convertirNumero(num) {
+  if (num % 2 === 0) {
+      return num.toString(2);
+  } else {
+      return num.toString(16);
+  }
 }
+
+// Ejemplo de uso
+let numero = 15;
+let resultado = convertirNumero(numero);
+console.log(resultado);
